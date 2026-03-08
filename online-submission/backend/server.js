ecfff8d3ecc5ea1ad72322c02a74e8f3270f0f2a
cabbages567 <2726933662@qq.com>
2026-03-08 20:20:53 +0800

    Update server.js

--- online-submission/backend/server.js
+++ online-submission/backend/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'submission_db',
   waitForConnections: true,
